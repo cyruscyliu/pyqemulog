b06afc0500f5f20ad2bac7f2ec4c32ea0b6095b8
cyrus <cyruscyliu@gmail.com>
2020-01-02 23:19:21 +0800

    add basic mips support

--- pyqemulog.py
+++ pyqemulog.py
@@ -21,9 +21,65 @@ class PQLI(object):
     def load_cpurf(self, dump=True):
         pass
 
-    @abc.abstractmethod
     def load_in_asm(self, dump=True):
-        pass
+        """
+        ----------------                                1
+        IN:                                             2
+        0x00000000:  e3a00000  mov      r0, #0          3
+        0x00000004:  e59f1004  ldr      r1, [pc, #4]    4
+        0x00000008:  e59f2004  ldr      r2, [pc, #4]    4
+        0x0000000c:  e59ff004  ldr      pc, [pc, #4]    4
+                                                        4 (end)
+        """
+        ln = 0  # ln number
+        bbs = {}  # to be returned
+
+        def parse_in_asm(line):
+            things = line.strip().split()
+            address = things[0][2:-1]  # remove 0x and :
+            raw = things[1]
+            opcode = things[2]
+            operand = things[3:]
+            offset = ln + 1
+            return offset, address, raw, opcode, operand
+
+        with open(self.path_to_qemulog) as f:
+            state = 0
+            for line in f:
+                if state == 0 and line.startswith('---'):
+                    state = 1
+                if state == 3:
+                    offset, address, raw, opcode, operand = parse_in_asm(line)
+                    bb_id = address
+                    new_bb = {'in': address, 'chained': False, 'instructions': [
+                        {'ln': offset, 'address': address, 'raw': raw, 'opcode': opcode, 'operand': operand}]}
+                    if bb_id in bbs:
+                        chained_bb = bbs[bb_id]
+                        while chained_bb['chained'] and 'next' in chained_bb:
+                            chained_bb = chained_bb['next']
+                        chained_bb['chained'] = True
+                        chained_bb['next'] = new_bb
+                    else:
+                        new_bb['chained'] = False
+                        bbs[bb_id] = new_bb
+                if state == 4 and len(line.strip()):
+                    offset, address, raw, opcode, operand = parse_in_asm(line)
+                    new_bb['instructions'].append(
+                        {'ln': offset, 'address': address, 'raw': raw, 'opcode': opcode, 'operand': operand})
+                if state in [1, 2, 3]:
+                    state += 1
+                if state == 4 and not len(line.strip()):
+                    new_bb['size'] = len(new_bb['instructions'])
+                    state = 0
+                ln += 1
+
+        self.bbs = bbs
+        if not dump:
+            return
+
+        # dump
+        with open('in_asm.json', 'w') as f:
+            json.dump(bbs, f)
 
     @abc.abstractmethod
     def get_pc(self, cpurf):
@@ -81,65 +137,6 @@ class PQL_AARCH32(PQLI):
 
     def get_pc(self, cpurf):
         return cpurf['register_files']['R15']
-
-    def load_in_asm(self, dump=True):
-        """
-        ----------------                                1
-        IN:                                             2
-        0x00000000:  e3a00000  mov      r0, #0          3
-        0x00000004:  e59f1004  ldr      r1, [pc, #4]    4
-        0x00000008:  e59f2004  ldr      r2, [pc, #4]    4
-        0x0000000c:  e59ff004  ldr      pc, [pc, #4]    4
-                                                        4 (end)
-        """
-        ln = 0  # ln number
-        bbs = {}  # to be returned
-
-        def parse_in_asm(line):
-            things = line.strip().split()
-            address = things[0][2:-1]  # remove 0x and :
-            raw = things[1]
-            opcode = things[2]
-            operand = things[3:]
-            offset = ln + 1
-            return offset, address, raw, opcode, operand
-
-        with open(self.path_to_qemulog) as f:
-            state = 0
-            for line in f:
-                if state == 0 and line.startswith('---'):
-                    state = 1
-                if state == 3:
-                    offset, address, raw, opcode, operand = parse_in_asm(line)
-                    bb_id = address
-                    new_bb = {'in': address, 'chained': False, 'instructions': [
-                        {'ln': offset, 'address': address, 'raw': raw, 'opcode': opcode, 'operand': operand}]}
-                    if bb_id in bbs:
-                        chained_bb = bbs[bb_id]
-                        while chained_bb['chained'] and 'next' in chained_bb:
-                            chained_bb = chained_bb['next']
-                        chained_bb['chained'] = True
-                        chained_bb['next'] = new_bb
-                    else:
-                        new_bb['chained'] = False
-                        bbs[bb_id] = new_bb
-                if state == 4 and len(line.strip()):
-                    offset, address, raw, opcode, operand = parse_in_asm(line)
-                    new_bb['instructions'].append(
-                        {'ln': offset, 'address': address, 'raw': raw, 'opcode': opcode, 'operand': operand})
-                if state in [1, 2, 3]:
-                    state += 1
-                if state == 4 and not len(line.strip()):
-                    new_bb['size'] = len(new_bb['instructions'])
-                    state = 0
-                ln += 1
-
-        if not dump:
-            return bbs
-        # dump
-        with open('in_asm.json', 'w') as f:
-            json.dump(bbs, f)
-        self.bbs = bbs
 
     def load_cpurf(self, dump=True):
         """
@@ -232,13 +229,14 @@ class PQL_AARCH32(PQLI):
                     cpurf_id += 1
                 ln += 1
 
+        self.cpurfs = cpurfs
+
         if not dump:
-            return cpurfs
+            return
+
         # dump
         with open('cpu.json', 'w') as f:
             json.dump(cpurfs, f)
-
-        self.cpurfs = cpurfs
 
 
 class PQL_MIPS32(PQLI):
@@ -246,13 +244,71 @@ class PQL_MIPS32(PQLI):
         super().__init__(endian, path_to_qemulog)
 
     def load_cpurf(self, dump=True):
-        pass
+        """
+        pc=0x80005d0c HI=0x00000000 LO=0x00000000 ds 0090 80005d0c 0        1 <- 0
+        GPR00: r0 00000000 at 1000001f v0 00000000 v1 00000000              2
+        GPR04: a0 00000000 a1 00000000 a2 00000000 a3 00000000              3
+        GPR08: t0 80005d0c t1 00000000 t2 00000000 t3 00000000              4
+        GPR12: t4 00000000 t5 00000000 t6 00000000 t7 00000000              5
+        GPR16: s0 00000000 s1 00000000 s2 00000000 s3 00000000              6
+        GPR20: s4 00000000 s5 00000000 s6 00000000 s7 00000000              7
+        GPR24: t8 00000000 t9 00000000 k0 00000000 k1 00000000              8
+        GPR28: gp 00000000 sp 00000000 s8 00000000 ra 00000000              9
+        CP0 Status  0x10400000 Cause   0x00000000 EPC    0x00000000         10
+            Config0 0x80000482 Config1 0x9e190c8f LLAddr 0x0000000000000000 11
+            Config2 0x80000000 Config3 0x00000c20                           12
+            Config4 0x00000000 Config5 0x00000000                           13
+        """
+        ln = 0
+        cpurfs = {}
 
-    def load_in_asm(self, dump=True):
-        pass
+        def parse_rfs(line, ref=4, off=1):
+            things = line.strip().split()
+            rfs = {}
+            for i in range(0, ref):
+                rfs[things[off + 2 * i]] = things[off + 1 + 2 * i]
+            offset = ln + 1
+            return offset, rfs
+
+        cpurf_id = 0
+        with open(self.path_to_qemulog) as f:
+            state = 0
+            for line in f:
+                if state == 0 and line.startswith('pc='):
+                    state = 1
+                    offset, rfs = ln + 1, {'pc': line.strip().split()[-2]}
+                    cpurfs[cpurf_id] = {'id': cpurf_id, 'ln': offset, 'register_files': rfs}
+                if state in [2, 3, 4, 5, 6, 7, 8, 9]:
+                    _, rfs = parse_rfs(line, ref=4, off=1)
+                    for rf_name, rf_value in rfs.items():
+                        cpurfs[cpurf_id]['register_files'][rf_name] = rf_value
+                if state in [10]:
+                    _, rfs = parse_rfs(line, ref=3, off=1)
+                    for rf_name, rf_value in rfs.items():
+                        cpurfs[cpurf_id]['register_files'][rf_name] = rf_value
+                if state in [11]:
+                    _, rfs = parse_rfs(line, ref=3, off=0)
+                    for rf_name, rf_value in rfs.items():
+                        cpurfs[cpurf_id]['register_files'][rf_name] = rf_value
+                if state in [12, 13]:
+                    _, rfs = parse_rfs(line, ref=2, off=0)
+                    for rf_name, rf_value in rfs.items():
+                        cpurfs[cpurf_id]['register_files'][rf_name] = rf_value
+                if state in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
+                    state += 1
+                if state == 14:
+                    state = 0
+                    cpurf_id += 1
+                ln += 1
+
+        if not dump:
+            return
+
+        with open('cpu.json', 'w') as f:
+            json.dump(cpurfs, f)
 
     def get_pc(self, cpurf):
-        pass
+        return cpurf['register_files']['pc']
 
 
 def get_pql(arch, endian, path_to_qemulog):
